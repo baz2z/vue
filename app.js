66b8859ea0913c37aeaf1c30e498aaa5427be3c4
baz2z <sebastian@beechey-volz.de>
2021-01-10 21:31:23 +0100

    bad version

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        a: 0,
+        b: 0
     },
     methods:{
-        greet: function(time){
-            return "Good " + time + " " +this.name;
-        }
+
     }
 })
