070a8de6c8f15eb90494a356a6046a72ec545b7a
baz2z <sebastian@beechey-volz.de>
2021-01-10 20:51:37 +0100

    adding dataBinding

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        job: "Dev",
+        website: "https://vuejs.org/v2/guide/#Getting-Started", 
+        websiteTag:    '<a href="https://vuejs.org/v2/guide/#Getting-Started">google</a>'
     },
     methods:{
         greet: function(time){
             return "Good " + time + " " +this.name;
-        },
-
-        logName: function(){
-            console.log("name")
-        },
-        
-        logAge: function(){
-            console.log("age")
         }
     }
-})
+}) 
