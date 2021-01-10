2ebe13c04ae65ee781b50fd41d3dfde83eaf8d93
baz2z <sebastian@beechey-volz.de>
2021-01-10 20:21:16 +0100

    added tutorial

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
-        name: "Seb",
-        job: "Dev",
-        website: "https://vuejs.org/v2/guide/#Getting-Started", 
-        websiteTag:    '<a href="https://vuejs.org/v2/guide/#Getting-Started">google</a>'
+       age: 25,
+       x: 0,
+       y: 0
     },
     methods:{
-        greet: function(time){
-            return "Good " + time + " " +this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+
+        subtract: function(dec){
+            this.age -= dec;
+        },
+
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 })
