dc519f80c72c88b6565ec21958507eb976d0fc86
baz2z <sebastian@beechey-volz.de>
2021-01-10 20:59:15 +0100

    adding two way binding

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
-        name: "Seb",
+        name: "",
+        age:""
     },
     methods:{
         greet: function(time){
             return "Good " + time + " " +this.name;
-        },
-
-        logName: function(){
-            console.log("name")
-        },
-        
-        logAge: function(){
-            console.log("age")
         }
     }
 })
