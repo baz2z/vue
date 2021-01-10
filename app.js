b751093b16f4be341a949629e710c39926f35d74
baz2z <sebastian@beechey-volz.de>
2021-01-10 20:22:48 +0100

    Base from which to start always

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
-        job: "Dev",
-        website: "https://vuejs.org/v2/guide/#Getting-Started", 
-        websiteTag:    '<a href="https://vuejs.org/v2/guide/#Getting-Started">google</a>'
     },
     methods:{
         greet: function(time){
