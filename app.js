cce8ef0aa9f322c6050326a7419b9cee66de3c04
baz2z <sebastian@beechey-volz.de>
2021-01-10 21:41:39 +0100

    better with computed properties

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        age: 20,
         a: 0,
         b: 0
     },
-    methods:{
+    //with methods, vue doesnt know which one to run so it goes through all
+    /*
+    methods: {
+        addToA: function () {
+            console.log("add to a")
+            return this.a + this.age
+        },
+        
+        addToB: function () {
+            console.log("add to b")
+            return this.b + this.age
+        },
+        
+    },
+    */
 
+    //change functions to properties
+
+    computed:{
+        addToA: function () {
+            console.log("add to a")
+            return this.a + this.age
+        },
+        addToB: function () {
+            console.log("add to b")
+            return this.b + this.age
+        }   
     }
 })
