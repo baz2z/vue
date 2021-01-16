f854f99cc246a764045298fecfc727b3c854181e
baz2z <sebastian@beechey-volz.de>
2021-01-16 20:09:57 +0100

    dynamicCss

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        available: false,
+        nearby: false
     },
     methods:{
         greet: function(time){
             return "Good " + time + " " +this.name;
+        }
+    },
+
+    computed:{
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 })
