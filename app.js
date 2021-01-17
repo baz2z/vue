85b3e43453cb52731a581b7a0abf0f0ec593acf3
baz2z <sebastian@beechey-volz.de>
2021-01-17 10:24:57 +0100

    fninished game

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        health: 100,
+        ended: false
     },
-    
-    methods:{
 
+    methods:{
+        punch: function(){
+            this.health -= 10;
+            if(this.health <= 1){
+                this.ended = true;
+            }
+        },
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
     },
 
     computed:{
 
     }
-})
+}) 
