1dfed05ae0904053f31ae3d1e4807408d76b8c18
baz2z <sebastian@beechey-volz.de>
2021-01-25 21:22:53 +0100

    refs

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ new Vue({
     el: '#vue-app', //which one to mainpulate/root element
     data: {
         name: "Seb",
+        output: "YOur fav food"
     },
     
     methods:{
-
+        readRefs: function(){
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
+        }
     },
 
     computed:{
