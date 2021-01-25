542bd9b039201af907fb17381997a00db96278aa
baz2z <sebastian@beechey-volz.de>
2021-01-25 21:11:39 +0100

    Beware of difference btw data as a function and data as a object

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-new Vue({
-    el: '#vue-app', //which one to mainpulate/root element
-    data: {
-        name: "Seb",
+var data = {
+    name: "Yoshi"
+}
+
+Vue.component("greeting", {
+    template: "<p> Hey there, I am {{name}}. <button v-on:click='changeName'> Change name </button></p>",
+    //data: function, that returns Object, not directly
+    //because there can be multiple instances --> if function: call by object
+    //function creates copy
+    // as property: call by value
+    data: function(){
+        return {
+            name: "Yoshi"
+        }
     },
-    
+
+ //   data: function(){
+ //       return data;
+ //   },
+
+
     methods:{
-
-    },
-
-    computed:{
-
+        changeName: function(){
+            this.name = "Pimmel"
+        }
     }
+})
+
+
+new Vue({
+    el: '#vue-app-one', //which one to mainpulate/root element
+    
+})
+
+
+new Vue({
+    el: '#vue-app-two', //which one to mainpulate/root element
+
 })
